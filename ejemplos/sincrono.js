9e0e50eb6f8626dc049b830b2821e9492c2aeb8f
josecervera20 <159598106+josecervera20@users.noreply.github.com>
2024-05-07 19:06:22 -0600

    Add files via upload

---
+++ ejemplos/sincrono.js
@@ -0,0 +1,20 @@
+function cuatro() {
+  console.log("cuatro");
+}
+
+function tres() {
+  console.log("tres");
+  cuatro();
+}
+
+function dos() {
+  console.log("dos");
+  tres();
+}
+
+function uno() {
+  console.log("uno");
+  dos();
+}
+
+uno();
